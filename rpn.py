38222e8f440d32b4c7ae9134f623291cf95beb66
Minh Tran <mtran0985@gmail.com>
2016-10-12 10:35:54 -0400

    Added test for exponent

--- rpn.py
+++ rpn.py
@@ -4,33 +4,33 @@ import operator
 
 
 operators = {
-	'+': operator.add,
-	'-': operator.sub,
-	'*': operator.mul,
-	'/': operator.truediv,
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
 }
 
 def calculate(myarg):
-	stack = list()
-	for token in myarg.split():
-		try:
-			token = int(token)
-			stack.append(token)
-		except ValueError:
-			function = operators[token]
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			result = function(arg1, arg2)
-			stack.append(result)
-		print(stack)
-	if len(stack) != 1:
-		raise TypeError("Too many parameters")
-	return stack.pop()
+    stack = list()
+    for token in myarg.split():
+	try:
+            token = int(token)
+	    stack.append(token)
+    	except ValueError:
+	    function = operators[token]
+	    arg2 = stack.pop()
+	    arg1 = stack.pop()
+	    result = function(arg1, arg2)
+	    stack.append(result)
+	print(stack)
+    if len(stack) != 1:
+	raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
-	while True:
-		result = calculate(input("rpn calc> "))
-		print("Result: ", result)
+    while True:
+	result = calculate(input("rpn calc> "))
+	print("Result: ", result)
 
 if __name__ == '__main__':
-	main()
+    main()
