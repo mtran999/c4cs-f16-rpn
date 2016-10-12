47ab1b0036bb33cefa669fb377e4d78ba9b5cdd5
Minh Tran <mtran0985@gmail.com>
2016-10-12 10:39:25 -0400

    Fixed spaces and tabs

--- rpn.py
+++ rpn.py
@@ -13,24 +13,25 @@ operators = {
 def calculate(myarg):
     stack = list()
     for token in myarg.split():
-	try:
+        try:
             token = int(token)
-	    stack.append(token)
-    	except ValueError:
-	    function = operators[token]
-	    arg2 = stack.pop()
-	    arg1 = stack.pop()
-	    result = function(arg1, arg2)
-	    stack.append(result)
-	print(stack)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
     if len(stack) != 1:
-	raise TypeError("Too many parameters")
+        raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
     while True:
-	result = calculate(input("rpn calc> "))
-	print("Result: ", result)
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
+
